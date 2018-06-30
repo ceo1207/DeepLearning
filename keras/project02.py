166e60acfb95e91608d8fb5f7542bdf208665ea8
RexShen <water799@126.com>
2018-06-30 20:53:00 +0800

    Create project02.py

---
+++ keras/project02.py
@@ -0,0 +1,10 @@
+# use a pre-trained model file to predict
+from keras.models import load_model
+predict_model = load_model('mnist0630.h5')
+from keras.datasets import mnist
+(_, _), (test_x, test_y) = mnist.load_data()
+label = predict_model.predict(test_x[[0], :].reshape(1, 784))
+import matplotlib.pyplot as plt
+plt.imshow(test_x[0])
+import numpy as np
+print np.argmax(label)
